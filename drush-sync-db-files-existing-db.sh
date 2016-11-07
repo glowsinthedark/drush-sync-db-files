8a7eaeb39c19126418b1d84a50af52e75443d551
David Lohmeyer <vilepickle@gmail.com>
2016-11-07 13:25:23 -0500

    Add script for existing DB that doesn't include DB credentials at all.

--- drush-sync-db-files-existing-db.sh
+++ drush-sync-db-files-existing-db.sh
@@ -1,12 +1,14 @@
 #############################################################################################
-# MySQL and File Sync Drush Sync Script
+# MySQL Drush Alias Database Loader Scripts
 #
 # By David Lohmeyer
 # vilepickle@gmail.com
-# 12/24/15 Version 1.0
+# 11/7/2016 Version 1.1
 #
-# THIS SCRIPT ASSUMES ~/.drush/site.aliases.drushrc.php IS FILLED OUT PROPERLY! SEE DEMO AT END
+# THIS SCRIPT ASSUMES ~/.drush/aliases.drushrc.php IS FILLED OUT PROPERLY! SEE DEMO AT END
 # OF FILE.
+#
+# THIS SCRIPT ASSUMES A DATABASE ALREADY EXISTS.
 #
 #############################################################################################
 
@@ -14,13 +16,6 @@ DO_FILES=1
 DO_DATABASE=1
 DRUSHSRC="@site.prod"
 DRUSHDEST="@site.local"
-
-# DATABASE CREDS
-SERVER="127.0.0.1"
-PORT="3306"
-USER="root"
-PW="overworked"
-DATABASE="your_db_name"
 
 #############################################################################################
 
@@ -31,37 +26,22 @@ DATABASE="your_db_name"
 # No need to edit below this line.
 
 if [ $DO_DATABASE = 1 ]
-	then
-		# Check to see if database exists
-		DBS=`mysql -u$USER -p$PW -h $SERVER -Bse 'show databases'| egrep -v 'information_schema|mysql'`
-		FOUNDDB=0
-		for db in $DBS; do
-			if [ $db = $DATABASE ]
-				then
-					FOUNDDB=1
-			fi
-		done
-		if [ $FOUNDDB = 0 ]
-			then
-				# Create a new database
-				echo "Didn't find the database, creating '$DATABASE'..."
-				mysqladmin -u$USER -p$PW -h $SERVER --port $PORT create $DATABASE
-		fi
-		# Sync the DB
-		echo "Removing old DB tables and syncing '$DATABASE' with defined drush aliases..."
-		drush $DRUSHDEST sql-drop -y
-		drush sql-sync $DRUSHSRC $DRUSHDEST -y
-	else
-		echo 'Database not synced...'
+  then
+    # Sync the DB
+    echo "Syncing '$DATABASE' with defined drush aliases..."
+    drush $DRUSHDEST sql-drop -y
+    drush sql-sync $DRUSHSRC $DRUSHDEST -y
+  else
+    echo 'Database not synced...'
 fi
 
 
 if [ $DO_FILES = 1 ]
-	then
-		echo 'Syncing files from source with drush rsync...'
-		drush -y rsync $DRUSHSRC:%files/ $DRUSHDEST:%files
-	else
-		echo 'Files not synced...'
+  then
+    echo 'Syncing files from source with drush rsync...'
+    drush -y rsync $DRUSHSRC:%files/ $DRUSHDEST:%files
+  else
+    echo 'Files not synced...'
 fi
 
 # SAMPLE ~/.drush/site.aliases.drushrc.php
@@ -90,4 +70,3 @@ fi
 #     '%files' => $remote_site_root . 'sites/yoursite.com/files'
 #   ),
 # );
-
